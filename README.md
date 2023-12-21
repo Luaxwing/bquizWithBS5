91863065b434582a03a2a5b8ea4bae7c467b642a
Luaxwing <nick890111@gmail.com>
2023-12-21 13:33:01 +0800

    20231221:import//css/js/jq

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 homework-用BS5/CSS重新編排乙級題組一內容
 
 20231221:init env
+         import css/js/jq
---
+++ env/js/js.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+$(document).ready(function(e) {
+    $(".mainmu").mouseover(
+		function()
+		{
+			$(this).children(".mw").stop().show()
+		}
+	)
+	$(".mainmu").mouseout(
+		function ()
+		{
+			$(this).children(".mw").hide()
+		}
+	)
+});
+function lo(x)
+{
+	location.replace(x)
+}
+// open
+function op(x,y,url)
+{
+	$(x).fadeIn()
+	if(y)
+	$(y).fadeIn()
+	if(y&&url)
+	$(y).load(url)
+}
+// close
+function cl(x)
+{
+	$(x).fadeOut();
+}
